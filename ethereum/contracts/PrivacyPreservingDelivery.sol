e8d00c78e5d61ba59d554707b18a12314ee6a47d
Majid Joseph <majid.joseph7@gmail.com>
2021-07-21 19:02:11 -0400

    finished contract

--- ethereum/contracts/PrivacyPreservingDelivery.sol
+++ ethereum/contracts/PrivacyPreservingDelivery.sol
@@ -4,38 +4,67 @@ pragma solidity ^0.8.6;
 
 contract PrivacyPreservingDelivery {
 
-    address owner;
-    address deliveryAgent;
-    mapping(string => string) public deliveryAddress;
+    enum Stages {
+        InitialCommit,
+        FirstReveal,
+        SecondReveal,
+        ThirdReveal,
+        FourthReveal,
+        Delivered
+    }
     
-    modifier restricted(string memory agentAddress) {
-        require(msg.sender == deliveryAgent);
+    struct CommitChoice {
+        string commitment;
+        uint64 block;
+        bool revealed;
+    }
+    
+    address owner;
+    CommitChoice[] public commits;
+    Stages stage;
+    uint64 commitIndex;
+    
+    modifier restricted() {
+        require(msg.sender == owner);
         _;
     }
     
-    constructor(string[] memory fullAddress) {
+    constructor(string memory dataHash1, string memory dataHash2, string memory dataHash3, string memory dataHash4) {
         owner = msg.sender;
-        deliveryAddress["Country"] = fullAddress[0];
-        deliveryAddress["Province"] = fullAddress[1];
-        deliveryAddress["City"] = fullAddress[2];
-        deliveryAddress["StreetName"] = fullAddress[3];
-        deliveryAddress["StreetAddress"] = fullAddress[4];
-        deliveryAddress["PostalCode"] = fullAddress[5];
-        deliveryAddress["UnitNumber"] = fullAddress[6];    
+        stage = Stages.InitialCommit;
+        commitIndex = 0;
+        
+        commit(dataHash1);
+        commit(dataHash2);
+        commit(dataHash3);
+        commit(dataHash4);
+        
+        stage = Stages.FirstReveal;
     }
     
-    //2
-    function registerDeliveryAgent() public {
-        // ...
+    function commit(string memory dataHash) public restricted{
+        
+        require(stage == Stages.InitialCommit);
+        
+        CommitChoice memory commitChoice = CommitChoice(dataHash, uint64(block.number), false);
+        commits.push(commitChoice);
     }
     
-    //1
-    function getNextAddressPiece(string memory deliveryAgentAddress) public restricted(deliveryAgentAddress){
-        // ...
+    function decrypt(bytes32 hashData) public view restricted returns(bytes32) {
     }
     
-    //to-do
-    // function getDeliveryAgents(ordernumber) public view returns (DeliveryAgent memory){
-    //     if(ordernumber == this.ordernumber) return deliveryAgent;
-    // }
+    function reveal() public payable returns(string memory){
+        require(stage == Stages.FirstReveal || stage == Stages.SecondReveal || stage == Stages.ThirdReveal || stage == Stages.FourthReveal);
+        require(commitIndex < commits.length);
+        require(commits[commitIndex].revealed == false);
+        
+        commits[commitIndex].revealed = true;
+        
+        if(stage == Stages.FirstReveal) stage = Stages.SecondReveal;
+        else if(stage == Stages.SecondReveal) stage = Stages.ThirdReveal;
+        else if(stage == Stages.ThirdReveal) stage = Stages.FourthReveal;
+        else if(stage == Stages.FourthReveal) stage = Stages.Delivered;
+        
+        return commits[commitIndex++].commitment;
+    }
 }
